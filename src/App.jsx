53f1f6cc3ae844574d4220b824dd30d33ddcf245
Simon Lau <simon.lau.sg@gmail.com>
2022-09-12 11:48:16 +0800

    add debug and fetch for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import debug from "debug";
+
+const log = debug("holidays:client:App");
+const SERVER = import.meta.env.VITE_SERVER;
+
+localStorage.debug = "holidays:*";
+
+fetch(SERVER)
+  .then((response) => response.json())
+  .then((data) => log(data));
+
 function App() {
   return (
     <div>
